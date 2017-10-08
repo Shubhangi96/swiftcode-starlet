e2e6a038eeb9845430f12d3f800d965503c0db18
Live session user <vasishta.shubhangi@gmail.com>
2017-10-08 05:33:59 +0000

    added classes

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,33 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+
+$scope.messages=[
+{
+'sender':'USER',
+'text':'hi',
+'time':'10:01 AM'
+
+},
+{
+'sender':'BOT',
+'text':'what can i do for you',
+'time':'10:01 AM'
+
+},
+{
+'sender':'USER',
+'text':'News about north korea',
+'time':'10:01 AM'
+
+},
+{
+'sender':'BOT',
+'text':'invalid',
+'time':'10:02 AM'
+
+}
+];
+
+
 });
