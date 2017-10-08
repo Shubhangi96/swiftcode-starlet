aa020f79f87cce96f5702a33015f15b22ce4767a
Live session user <vasishta.shubhangi@gmail.com>
2017-10-08 07:37:13 +0000

    done styling

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,32 +2,22 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
 
-$scope.messages=[
-{
-'sender':'USER',
-'text':'hi',
-'time':'10:01 AM'
+$scope.messages=[];
 
-},
-{
-'sender':'BOT',
-'text':'what can i do for you',
-'time':'10:01 AM'
-
-},
-{
-'sender':'USER',
-'text':'News about north korea',
-'time':'10:01 AM'
-
-},
-{
-'sender':'BOT',
-'text':'invalid',
-'time':'10:02 AM'
-
-}
-];
+var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+    exampleSocket.onmessage  =   function  (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+    $scope.sendMessage = function () {
+            exampleSocket.send($scope.userMessage);
+            $scope.userMessage = '';
+        };
+        $scope.trust=$sce.trustAsHtml;
 
 
 });
